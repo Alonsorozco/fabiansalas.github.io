88b0cdd3dc7a26930593c25b3f468af7355ca1a5
Alonsorozco <83556353+Alonsorozco@users.noreply.github.com>
2024-11-10 21:37:48 -0300

    add contacto

--- asset/js/script.js
+++ asset/js/script.js
@@ -23,3 +23,22 @@ $(function(){
     $('[data-toggle="tooltip"]').tooltip() 
   
   }); 
+
+
+    // Obtener el número de visitas desde localStorage
+    let visitCount = localStorage.getItem('visitCount');
+
+    // Si no hay un contador guardado, inicialízalo en 0
+    if (!visitCount) {
+        visitCount = 0;
+    }
+
+    // Incrementar el contador de visitas
+    visitCount++;
+
+    // Guardar el nuevo valor en localStorage
+    localStorage.setItem('visitCount', visitCount);
+
+    // Mostrar el contador en la página
+    document.getElementById('visit-count').textContent = visitCount;
+
